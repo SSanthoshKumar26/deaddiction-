bbb1fd88a949d9acdb3f222db0f4467751b3f2cc
SSanthoshKumar26 <santolbs26@gmail.com>
2026-02-18 00:33:56 +0530

    new fr

--- Frontend/src/components/MobileSidebar.jsx
+++ Frontend/src/components/MobileSidebar.jsx
@@ -1,3 +1,4 @@
+import React, { useEffect } from 'react';
 import ReactDOM from 'react-dom';
 import { Link, useLocation, useNavigate } from 'react-router-dom';
 import { motion, AnimatePresence } from 'framer-motion';
